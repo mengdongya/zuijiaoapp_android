c64e559d5d9c960828fa78001524961c2d3f15e2
Chen Hao <chenhao@zuijiao.net>
2015-05-18 17:35:24 +0800

    IRouterSocial using web interface instead

--- model/src/main/java/com/zuijiao/android/zuijiao/network/IRouterAccount.java
+++ model/src/main/java/com/zuijiao/android/zuijiao/network/IRouterAccount.java
@@ -47,7 +47,6 @@ public interface IRouterAccount {
     @GET("/users/v2/person/{id}")
     void fetchUserInfoByIdentifier(@Path("id") Integer identifier, Callback<User> callback);
 
-    // FIXME: should use /account/v2/mobile/info
     @GET("/account/v2/info")
     void fetchMyInfo(Callback<User> callback);
 

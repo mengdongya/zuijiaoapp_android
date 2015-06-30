cb796bd730a3f09f294a3c79dce30f705ef7f910
mengdongya <mengdongya@zuijiao.net>
2015-06-30 09:55:58 +0800

    'jj'

--- model/src/main/java/com/zuijiao/android/zuijiao/model/OrderAuth.java
+++ model/src/main/java/com/zuijiao/android/zuijiao/model/OrderAuth.java
@@ -11,7 +11,7 @@ public class OrderAuth {
     private String apiVersion;
     @SerializedName("kind")
     private String kind;
-    @SerializedName("data")
+    @SerializedName("data")//
     private Data data;
 
     public String getAppId() {
